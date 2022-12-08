41df3c56ca9bf7d79adcd5526857da34f5815fdb
TimofeyTst <yarik.mist@gmail.com>
2022-12-08 18:32:42 +0300

    Try to make all by myself

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+  before_action :require_login
+
+  private
+
+  def require_login
+    return if signed_in?
+
+    redirect_to session_new_path, notice: 'Please sign in'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'home#show'
+
+  get 'session/new'
+  post 'session/create'
+  delete 'session/destroy'
+
+  resources :users, only: %i[new create destroy edit update show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
