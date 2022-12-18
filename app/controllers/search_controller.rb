13dfc5a8655796bfa75518b81b0c7359f6fa49c7
TimofeyTst <yarik.mist@gmail.com>
2022-12-19 01:09:06 +0300

    Styles for chat & other

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,18 @@ class SearchController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @q = Post.ransack(params[:q])
-
-    @posts = @q.result(distinct: true)
+    @posts = Post.ransack(post_params).result(distinct: true)
 
     @posts_by_three = arr_by_three_columns(@posts)
+  end
+
+  private
+
+  def user_params
+    { 'username_start' => params[:q] }
+  end
+
+  def post_params
+    { 'tags_i_cont_any' => params[:q] }
   end
 end
